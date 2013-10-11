7caeb5c1a7621592bd688befb0433b1c9020db82
Dmitry Skobelev <dmitry.skobelev@softensity.com>
2013-10-11 17:04:21 +0300

    Some entities and basic logic added

--- source/CoffeeBank/Coffee.Entities/CreditRequest.cs
+++ source/CoffeeBank/Coffee.Entities/CreditRequest.cs
@@ -2,10 +2,12 @@
 {
     public class CreditRequest
     {
-        public long Sum { get; set; } //In belorussian rubles only
+        public decimal Sum { get; set; } //In belorussian rubles only
 
         public PassportInfo PassportInfo { get; set; }
 
         public SalaryInfo SalaryInfo { get; set; }
+
+        //...some other info
     }
 }
