55686539f68fdf552e872604062cbf1b7842a770
demidov91 <dmitryDemidov91@gmail.com>
2014-01-09 18:34:51 +0300

    Membership is now detecting 'BankWorker's. 4 tons of shit is removed.

--- source/CoffeeBank/Coffee.WebUi/Controllers/CreditController.cs
+++ source/CoffeeBank/Coffee.WebUi/Controllers/CreditController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 using Coffee.Entities;
 using Coffee.Repository;
-
+using Coffee.WebUi.Scripts;
 
 namespace Coffee.WebUi.Controllers
 {
@@ -16,12 +16,11 @@ namespace Coffee.WebUi.Controllers
         public ActionResult List(string passportNumber)
         {
             List<Credit> creditsToShow = RepoFactory.GetCreditsRepo().GetAllIssuedCredits();
-            bool isSimpleUser = User.Identity.Name != "BankWorker";                             ///kill this part of code
             if (passportNumber != null)
             {
                 creditsToShow = creditsToShow.FindAll(x => x.Passport.PassportNumber.StartsWith(passportNumber));
             }
-            else if (isSimpleUser) {
+            else if (MembershipHelper.IsExternalUser(User)) {
                 creditsToShow = creditsToShow.FindAll(x => x.User == User.Identity.Name);
             }
             return View("~/Views/RealCredit/List.cshtml", creditsToShow);
---
+++ source/CoffeeBank/Coffee.WebUi/Scripts/MembershipHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.WebUi.Scripts
+{
+    public class MembershipHelper
+    {
+        public static bool IsExternalUser(System.Security.Principal.IPrincipal user) {
+            return !(user.IsInRole("Cashier") || user.IsInRole("Clerk") || user.IsInRole("Committee"));
+        } 
+    }
+}
